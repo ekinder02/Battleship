e45705d6c6f578c0f774e45324eb323bad091fd5
ekinder02 <110261879+ekinder02@users.noreply.github.com>
2023-11-29 09:45:16 -0600

    playerClass Updated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import player
-board = [[],[],[],[],[],[],[],[],[],[],[],[]]*12
-
-print(board)
+player1 = player.Player([],[],100,1)
+player1.createCleanBoard()
+for i in player1.board:
+    print(i)
--- player.py
+++ player.py
@@ -1,7 +1,32 @@
 class Player:
-    def __init__(self, shipList, board, cash, number):
+    def __init__(self, shipList, board, cash, number, powerUps):
         self.shipList = shipList
         self.board = board
         self.cash = cash
         self.number = number
+        self.powerUps = powerUps
+    def createCleanBoard(self):
+        for i in range(12):
+            self.board.append([["-"]]*12)
+    def createShipList(self):
+        return(0)
+    def placeShip(self):
+        for i in self.shipList:
+            coord = input("Place the bottom left corner of your ship: (A1) ").upper()
+            x,y = ord(coord[0])-65, int(coord[1])-1
+            allignment = input("Vertical or Horizontal: ").lower()
+            if allignment == "vertical":
+                for j in range(i.length):
+                    self.board[y+j][x] = "S"
+            elif allignment == "horizontal":
+                for j in range(i.length):
+                    self.board[y][x+j] = "S"
+    def shootMissile(self, enemy):
+        coord = input("Shoot your missile: (A1) ").upper()
+        x,y = ord(coord[0])-65, int(coord[1])-1
+        if enemy.board[y][x] == "S":
+            enemy.board[y][x] = "H"
+            print("Hit!")
+        else:
+            print("Miss!")
     
