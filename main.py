4c413b800bb31f48c38f57f086d59b91c45907a8
ekinder02 <110261879+ekinder02@users.noreply.github.com>
2023-11-29 09:24:19 -0600

    player

---
+++ main.py
@@ -0,0 +1,4 @@
+import player
+board = [[],[],[],[],[],[],[],[],[],[],[],[]]*12
+
+print(board)
---
+++ player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, shipList, board, cash, number):
+        self.shipList = shipList
+        self.board = board
+        self.cash = cash
+        self.number = number
+    
