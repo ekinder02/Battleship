1f248b9cd3be3b3007e6954a9a564a0fda6a93f7
816ford <816.ford@gmail.com>
2023-12-04 09:30:13 -0600

    Fixed place ship and shoot missile

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ player2.createCleanBoard()
 def main():
     for i in player1.board:
         print(i)
-        
     player1.createShipList()
     player2.createShipList()
     player1.placeShip()
     player2.placeShip()
     while True:
+        player1.buyPowerUps()
         power1 = player1.usePowerUp()
         if power1 != False:
             player1.choosePowerUp()
@@ -22,11 +22,11 @@ def main():
         if player1.checkWin():
             print("Player 1 wins!")
             break
+        player2.buyPowerUps()
         player2.usePowerUp()
         player2.shootMissile(player1)
         if player2.checkWin():
             print("Player 2 wins!")
             break
-        print(player1.cash)
 
 main()
--- player.py
+++ player.py
@@ -9,29 +9,51 @@ class Player:
     def createCleanBoard(self):
         board = []
         for i in range(12):
-            board.append([["-"]]*12)
+            board.append(["-"]*12)
         self.board = board
     def createShipList(self):
         self.shipList.append(ship.Ship(5,5,[]))
-        self.shipList.append(ship.Ship(4,4,[]))
-        self.shipList.append(ship.Ship(3,3,[]))
-        self.shipList.append(ship.Ship(3,3,[]))
-        self.shipList.append(ship.Ship(2,2,[]))
+        #self.shipList.append(ship.Ship(4,4,[]))
+        #self.shipList.append(ship.Ship(3,3,[]))
+        #self.shipList.append(ship.Ship(3,3,[]))
+        #self.shipList.append(ship.Ship(2,2,[]))
     def placeShip(self):
         for i in self.shipList:
-            coord = input(f"Place the bottom left corner of your {i.length} length ship: (A1) ").upper()
-            y,x = int(coord[1])-1, ord(coord[0])-65
-            allignment = input("Vertical or Horizontal: ").lower()
-            if allignment == "vertical":
-                for j in range(i.length):
-                    self.board[y-j][x] = "S"
-                    i.coordinates.append([y-j,x])
-            elif allignment == "horizontal":
-                for j in range(i.length):
-                    self.board[y][x+j] = "S"
-                    i.coordinates.append([y,x+j])
-            for i in self.board:
-                print(i)
+            coord = input(f"Place the bottom left corner of your {i.length} length ship: (A1 - L12) ").upper()
+            if len(coord) == 2:
+                y,x = int(coord[1])-1, ord(coord[0])-65
+                allignment = input("Vertical or Horizontal: ").lower()
+                if allignment == "vertical":
+                    for j in range(i.length):
+                        self.board[y-j][x] = "S"
+                        i.coordinates.append([y-j,x])
+                elif allignment == "horizontal":
+                    for j in range(i.length):
+                        self.board[y][x+j] = "S"
+                        i.coordinates.append([y,x+j])
+                for row in self.board:
+                    for i in range(12):
+                        if i != 11:
+                            print(row[i], end=" ")
+                        else:
+                            print(row[i])
+            else:
+                y,x = int(coord[1:])-1, ord(coord[0])-65
+                allignment = input("Vertical or Horizontal: ").lower()
+                if allignment == "vertical":
+                    for j in range(i.length):
+                        self.board[y-j][x] = "S"
+                        i.coordinates.append([y-j,x])
+                elif allignment == "horizontal":
+                    for j in range(i.length):
+                        self.board[y][x+j] = "S"
+                        i.coordinates.append([y,x+j])
+                for row in self.board:
+                    for i in range(12):
+                        if i != 11:
+                            print(row[i], end=" ")
+                        else:
+                            print(row[i])
     def usePowerUp(self):
         x = input("Do you want to use a power up? (Y/N) ").lower()
         if x == "y":
@@ -74,11 +96,68 @@ class Player:
                             self.cash += 100
         else:
             print("Miss!")
-    
-
+    def shootMissile(self, enemy):
+        coord = input("Shoot your missile: (A1) ").upper()
+        if len(coord) == 2:
+            x,y = ord(coord[0])-65, int(coord[1])-1
+            if enemy.board[y][x] == "S":
+                enemy.board[y][x] = "H"
+                print("Hit!")
+                for i in enemy.shipList:
+                    for coord in i.coordinates:
+                        if coord == [y,x]:
+                            i.health -= 1
+                            if i.health == 0:
+                                print("Sunk!")
+                                self.cash += 100
+            else:
+                print("Miss!")
+        else:
+            x,y = ord(coord[0])-65, int(coord[1:])-1
+            if enemy.board[y][x] == "S":
+                enemy.board[y][x] = "H"
+                print("Hit!")
+                for i in enemy.shipList:
+                    for coord in i.coordinates:
+                        if coord == [y,x]:
+                            i.health -= 1
+                            if i.health == 0:
+                                print("Sunk!")
+                                self.cash += 100
+            else:
+                print("Miss!")
     def checkWin(self):
         for i in self.shipList:
             if i.checkHealth() == False:
                 return False
         return True
     
+    def buyPowerUps(self):
+        print("You have " + str(self.cash) + " cash.")
+        print("2x2 -> 10 cash")
+        print("UAV -> 25 cash")
+        print("Airstrike -> 50 cash")
+        print("Boat Upgrade -> 50 cash")
+        print("Move Boat -> 50 cash")
+        print("Two Moves -> 50 cash")
+        print("X Hit -> 50 cash")
+
+        z = input("Do you want to buy a power up? (Y/N) ").lower()
+        if z == "y":
+            x = input("Which power up do you want to buy? ").lower()
+            if x == "2x2":
+                self.buy2x2()
+            elif x == "uav":
+                self.buyUAV()
+            elif x == "airstrike":
+                self.buyAirstrike()
+            elif x == "boat upgrade":
+                self.buyBoatUpgrade()
+            elif x == "move boat":
+                self.buyMoveBoat()
+            elif x == "two moves":
+                self.buyTwoMoves()
+            elif x == "x hit":
+                self.buyXHit()
+        else:
+            return False
